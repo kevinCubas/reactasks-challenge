21780ee86df017f94d1ce6018b365780e161cd0a
unknown <61201131+kevinCubas@users.noreply.github.com>
2023-02-22 01:02:17 -0300

    feat: remove task function

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -38,6 +38,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newTasksList = tasks.filter(tasks => tasks.id !== id)
+    setTasks(newTasksList)
   }
 
   return (
